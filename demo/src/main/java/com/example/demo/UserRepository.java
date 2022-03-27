58371be8a8779e3f44a55c4fce9cdd30b5c305ae
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-03-27 14:07:03 +0200

    realizacion de consulta

--- demo/src/main/java/com/example/demo/UserRepository.java
+++ demo/src/main/java/com/example/demo/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface UserRepository extends CrudRepository<User, Long> {
-
+  @Query("select u from User u where u.name")
+  User findByName(String name);
 }
