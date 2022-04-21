ffdaae6d82ee97a527013e95590456f9bb9b5a5d
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-04-21 15:58:46 +0200

    funcion al borrar

--- demo/src/main/java/com/example/demo/controller/HeroController.java
+++ demo/src/main/java/com/example/demo/controller/HeroController.java
@@ -1,31 +1,43 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import com.example.demo.entity.Hero;
 import com.example.demo.repository.HeroRepository;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
-
+@RequestMapping("heroes")
 public class HeroController {
 
     @Autowired
     private HeroRepository repository;
 
-    @GetMapping(path = "/heroes")
+    @GetMapping("")
     public List<Hero> allHeroes() {
         return (List<Hero>) repository.findAll();
     }
+
+    @PostMapping("")
+    ResponseEntity<Hero> create(@RequestBody Hero hero) {
+        return ResponseEntity.ok(repository.save(hero));
+    }
+
+    @DeleteMapping("")
+    void delete(@RequestParam(required = true) Long id) {
+        repository.delete(repository.findById(id).get());
+    }
+
+    
 }
